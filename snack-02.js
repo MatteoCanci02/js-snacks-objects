31d05478763f0fdff8343daa2e0b2cd6b94946ee
MatteoCanci02 <matteocancilleri02@gmail.com>
2025-06-09 12:17:21 +0200

    ciclo for fine snack-02

--- snack-02.js
+++ snack-02.js
@@ -1,2 +1,16 @@
 console.log('Snack-02')
-const parole = ['Pippo','pluto','Paperino','TOPOLINO','ciaO']
+const parole = [prompt('scrivi una parola'),'pluto','Paperino','TOPOLINO','ciaO']
+
+const parolaFormattata = []
+
+for(let i = 0; i < parole.length; i++){
+    const elementoCorrente = parole[i]
+    // console.log(i, elementoCorrente)
+    let primaLettera = elementoCorrente.charAt(0).toUpperCase()
+    let resto = elementoCorrente.slice(1).toLowerCase()
+    parolaFormattata.push(primaLettera + resto)
+}
+
+console.log(parolaFormattata)
+
+
