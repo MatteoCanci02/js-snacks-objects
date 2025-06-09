3a88fd948f67c7a539ef8657a172915a4b1a26aa
MatteoCanci02 <matteocancilleri02@gmail.com>
2025-06-09 11:25:12 +0200

    Array automobili

---
+++ snack-01.js
@@ -0,0 +1,54 @@
+console.log('Snack-01')
+
+const automobili = [
+    {
+        marca: 'Ford'
+        modello: 'Fiesta'
+        Alimentazione: 'Metano'
+    }
+    {
+        marca: 'Fiat'
+        modello: 'Panda'
+        Alimentazione: 'Benzina'
+    }
+    {
+        marca: 'Lamborghini'
+        modello: 'Urus'
+        Alimentazione: 'Diesel'
+    }
+    {
+        marca: 'Fiat'
+        modello: 'Tipo'
+        Alimentazione: 'Benzina'
+    }
+    {
+        marca: 'Audi'
+        modello: 'A1'
+        Alimentazione: 'Diesel'
+    }
+    {
+        marca: 'Lancia'
+        modello: 'Y'
+        Alimentazione: 'Gpl'
+    }
+    {
+        marca: 'Ford'
+        modello: 'Kuga'
+        Alimentazione: 'Benzina'
+    }
+    {
+        marca: 'Tesla'
+        modello: 'Model-Y'
+        Alimentazione: 'Elettrico'
+    }
+    {
+        marca: 'Peugeot'
+        modello: '3008'
+        Alimentazione: 'Diesel'
+    }
+    {
+        marca: 'Audi'
+        modello: 'R8'
+        Alimentazione: 'Elettrico'
+    }
+]
