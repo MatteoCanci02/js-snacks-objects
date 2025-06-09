d7ebf16c810f4367eb6e7b6daf62accbf4511e95
MatteoCanci02 <matteocancilleri02@gmail.com>
2025-06-09 11:46:20 +0200

    ciclo for e if fine snack-01

--- snack-01.js
+++ snack-01.js
@@ -2,53 +2,76 @@ console.log('Snack-01')
 
 const automobili = [
     {
-        marca: 'Ford'
-        modello: 'Fiesta'
-        Alimentazione: 'Metano'
-    }
+        marca: 'Ford',
+        modello: 'Fiesta',
+        alimentazione: 'Metano',
+    },
     {
-        marca: 'Fiat'
-        modello: 'Panda'
-        Alimentazione: 'Benzina'
-    }
+        marca: 'Fiat',
+        modello: 'Panda',
+        alimentazione: 'Benzina',
+    },
     {
-        marca: 'Lamborghini'
-        modello: 'Urus'
-        Alimentazione: 'Diesel'
-    }
+        marca: 'Lamborghini',
+        modello: 'Urus',
+        alimentazione: 'Diesel',
+    },
     {
-        marca: 'Fiat'
-        modello: 'Tipo'
-        Alimentazione: 'Benzina'
-    }
+        marca: 'Fiat',
+        modello: 'Tipo',
+        alimentazione: 'Benzina',
+    },
     {
-        marca: 'Audi'
-        modello: 'A1'
-        Alimentazione: 'Diesel'
-    }
+        marca: 'Audi',
+        modello: 'A1',
+        alimentazione: 'Diesel',
+    },
     {
-        marca: 'Lancia'
-        modello: 'Y'
-        Alimentazione: 'Gpl'
-    }
+        marca: 'Lancia',
+        modello: 'Y',
+        alimentazione: 'Gpl',
+    },
     {
-        marca: 'Ford'
-        modello: 'Kuga'
-        Alimentazione: 'Benzina'
-    }
+        marca: 'Ford',
+        modello: 'Kuga',
+        alimentazione: 'Benzina',
+    },
     {
-        marca: 'Tesla'
-        modello: 'Model-Y'
-        Alimentazione: 'Elettrico'
-    }
+        marca: 'Tesla',
+        modello: 'Model-Y',
+        alimentazione: 'Elettrico',
+    },
     {
-        marca: 'Peugeot'
-        modello: '3008'
-        Alimentazione: 'Diesel'
-    }
+        marca: 'Peugeot',
+        modello: '3008',
+        alimentazione: 'Diesel',
+    },
     {
-        marca: 'Audi'
-        modello: 'R8'
-        Alimentazione: 'Elettrico'
-    }
+        marca: 'Audi',
+        modello: 'R8',
+        alimentazione: 'Elettrico',
+    },
 ]
+
+const Benzina = []
+const Diesel = []
+const Altro = []
+
+for(let i = 0; i < automobili.length; i++) {
+    const elementoCorrente = automobili[i]
+    // console.log(i, elementoCorrente)
+
+    if (elementoCorrente.alimentazione === 'Benzina'){
+        Benzina.push(elementoCorrente)
+    } else if (elementoCorrente.alimentazione === 'Diesel'){
+        Diesel.push(elementoCorrente)
+    } else {
+        Altro.push(elementoCorrente)
+    }
+}
+
+console.log(Benzina)
+console.log(Diesel)
+console.log(Altro)
+
+
